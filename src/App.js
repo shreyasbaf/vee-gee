79c5496a35107dfab478e21bd873b0186e9b3dc8
shreyasbaf <shreyas7bafna@gmail.com>
2022-05-07 12:14:28 +0530

    logo changed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import RouterMain from './pages/RouterMain';
 import styled from 'styled-components';
 import {Provider} from 'react-redux';
 import store from '../src/redux/store'
-
+import logo from './assets/images/logo.png'
 function App() {
   const Container = styled.div`
-  /* background: #67bc98; */
+  background-image: url('https://products.ls.graphics/mesh-gradients/images/23.-California_1.jpg');
   /* height: 100vh; */
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 return (
     <Provider store = {store}>
